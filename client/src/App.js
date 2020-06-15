1521f367313e6aae30328e9c3303d56d3b48e9f3
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-06-15 15:18:01 -0700

    Need to make a branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Switch, Route, Link } from 'react-router-dom'
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -26,9 +26,26 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className='app'>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div className='home'>
+        <Link to='/'>Home</Link>
+        <Link to='/movie-list'>Seleting a movie</Link>
+      </div>
+      {/* <div>Replace this Div with your Routes</div> */}
+      <Switch>
+        <Route path='/movie-list/:movieId'>
+        <Item items={movies} />
+        </Route>
+
+        <Route path='/movie-list'>
+        <ItemList items={movies} />
+        </Route>
+
+        <Route path='/'>
+
+        </Route>
+      </Switch>
     </div>
   );
 };
